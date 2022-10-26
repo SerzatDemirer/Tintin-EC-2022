f0a82e56303649ab8d6b6cc0576051a3b1d14f20
Serzat Demirer <serzat.demirer1@gmail.com>
2022-10-26 13:40:39 +0200

    feat: hero tintin text animation is completed and butun animation is implanted

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 const text = document.querySelector('.tintin-text');
 const strText = text.textContent;
 const splitText = strText.split("");
@@ -5,4 +10,22 @@ text.textContent = "";
 
 for (let i = 0; i < splitText.length; i++) {
     text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+    if (char === splitText.length) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
 }
